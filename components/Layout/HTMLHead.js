f97bacea5fea1b7fada546708b377ebc21893278
Vannelo <work@vannelo.com>
2024-02-02 12:32:40 -0600

    new description

--- components/Layout/HTMLHead.js
+++ components/Layout/HTMLHead.js
@@ -6,11 +6,11 @@ const HTMLHead = () => {
       <title>Vannelo | Web experiences artisan</title>
       <meta
         name="description"
-        content="I am a Full-Stack Developer/Software Engineer with 8+ years of
-          experience in web development, creating top quality websites with
-          cutting edge techonologies. I believe that success is in the
-          details, therefore, I always seek excellence in every project in
-          which I get involved."
+        content="Results-driven and highly skilled Fullstack Software Engineer with
+        a proven track record of success spanning eight years in the
+        industry. Adept at collaborating within multicultural teams on a
+        global scale, with experience contributing to the success of major
+        corporations."
       />
       <meta
         name="keywords"
@@ -21,11 +21,11 @@ const HTMLHead = () => {
       <meta property="og:title" content="Vannelo | Web experiences artisan" />
       <meta
         property="og:description"
-        content="I am a Full-Stack Developer/Software Engineer with 8+ years of
-          experience in web development, creating top quality websites with
-          cutting edge techonologies. I believe that success is in the
-          details, therefore, I always seek excellence in every project in
-          which I get involved."
+        content="Results-driven and highly skilled Fullstack Software Engineer with
+        a proven track record of success spanning eight years in the
+        industry. Adept at collaborating within multicultural teams on a
+        global scale, with experience contributing to the success of major
+        corporations."
       />
       <meta property="og:url" content="https://vannelo.com" />
       <meta property="og:image" content="https://vannelo.com/meta.jpg" />
