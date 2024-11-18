4eb647e25919d47c8ef976829a74a4c4af758e12
yira <yira@yiras-MacBook-Pro.local>
2024-11-17 20:17:09 -0600

    Changes 17 nov

--- components/Categories.js
+++ components/Categories.js
@@ -1,14 +1,15 @@
 const CATEGORIES = [
   { key: 1, name: "All" },
   { key: 2, name: "Front-End" },
-  { key: 3, name: "Back-End" },
-  { key: 4, name: "Databases" },
-  { key: 5, name: "Cloud" },
-  { key: 6, name: "Team Tools" },
-  { key: 7, name: "CMS" },
-  { key: 8, name: "CRM" },
-  { key: 9, name: "Graphic Design" },
-  { key: 10, name: "Music Production" },
+  { key: 3, name: "App" },
+  { key: 4, name: "Back-End" },
+  { key: 5, name: "Databases" },
+  { key: 6, name: "Cloud" },
+  { key: 7, name: "Work" },
+  { key: 8, name: "CMS" },
+  { key: 9, name: "CRM" },
+  { key: 10, name: "Design" },
+  { key: 11, name: "Music" },
 ];
 
 export default CATEGORIES;
